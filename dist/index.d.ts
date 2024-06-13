ea8fb1ddd7ba2e577dca72180c883cbfccf6cb5c
Jerome WANG immjs <informathemusic@gmail.com>
2024-06-13 02:30:26 +0200

    ooooooops i forgot to build.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,5 @@
-export {};
+import { Image } from './components/image';
+import { anyHandler } from './converters/any';
+import { sharpHandler } from './converters/sharp';
+import { defaultImg } from './converters/default';
+export { Image, anyHandler, sharpHandler, defaultImg };
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.defaultImg = exports.sharpHandler = exports.anyHandler = exports.Image = void 0;
+const image_1 = require("./components/image");
+Object.defineProperty(exports, "Image", { enumerable: true, get: function () { return image_1.Image; } });
+const any_1 = require("./converters/any");
+Object.defineProperty(exports, "anyHandler", { enumerable: true, get: function () { return any_1.anyHandler; } });
+const sharp_1 = require("./converters/sharp");
+Object.defineProperty(exports, "sharpHandler", { enumerable: true, get: function () { return sharp_1.sharpHandler; } });
+const default_1 = require("./converters/default");
+Object.defineProperty(exports, "defaultImg", { enumerable: true, get: function () { return default_1.defaultImg; } });
